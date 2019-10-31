ad736901230e1d6456d8d09d5862ddf1cdbcb0dc
Pau Masip <36194293+paumasip19@users.noreply.github.com>
2019-10-31 11:41:20 +0100

    Update
    - Project created
    - Working on Loby

---
+++ The Uncomfortable Man - Prototipo/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Camera cam;
+    public Vector3 movement;
+    public float moveSpeed;
+
+    private float horizontal;
+    private float vertical;
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void FixedUpdate()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        movement = (cam.transform.forward * vertical + cam.transform.right * horizontal).normalized * moveSpeed;
+
+        controller.SimpleMove(movement);
+    }
+}
